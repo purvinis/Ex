892b1ccbe3e9d98a9c7c4f0b7c0e206ac0a1ba83
Purvinis <g.purvinis@gmail.com>
2020-09-08 05:52:41 -1000

    work in progress

--- tasks.R
+++ tasks.R
@@ -17,7 +17,7 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 emmissionTot <-NEI$Emissions
 print(mean(is.na(emmissionTot)))   #make sure there are no NAs
-emByYr <- sapply(split(emmissionTot,NEI$year), mean)
+emByYr <- sapply(split(emmissionTot,NEI$year), sum)
 years <- unique(NEI$year)
 
 png(filename = "plot1.png")
@@ -25,13 +25,31 @@ png(filename = "plot1.png")
 bp <- barplot(emByYr, col = rgb( .5,0,.5,.2),
      xlab = "Year",
      ylab = "tons",
-     main = "Total PM2.5 emission in the U.S. \n from all sources by year")
-text(bp,emByYr*0.9,labels = round(emByYr,digits = 2))
+     main = "Total PM2.5 emission in the U.S. \n from all sources by year",
+     sub = "Bar plot showing total PM2.5 decreased from 1999 to 2008.")
+text(bp,emByYr*0.9,labels = round(emByYr,digits = 0))
 
 dev.off()
 
 # Q2.
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland 
-# (fips == "24510"\color{red}{\verb|fips == "24510"|}fips == "24510") 
+# (fips == "24510") 
 # from 1999 to 2008? Use the base plotting system to make a plot answering 
 # this question.
+
+baltimore <- subset(NEI,NEI$fips=="24510")
+emissionsBalt <-baltimore$Emissions
+print(mean(is.na(emmissionTot)))   #make sure there are no NAs
+emByYrBalt <- sapply(split(emissionsBalt,NEI$year), sum)
+years <- unique(NEI$year)
+
+png(filename = "plot1.png")
+
+bp <- barplot(emByYrBalt, col = rgb( 0,0.5,0.5,0.2),
+              xlab = "Year",
+              ylab = "tons",
+              main = "Total PM2.5 emission in Baltimore \n from all sources by year",
+              sub = "Bar plot showing total PM2.5 decreased from 1999 to 2008.")
+text(bp,emByYrBalt*0.9,labels = round(emByYrBalt,digits = 0))
+
+dev.off()
