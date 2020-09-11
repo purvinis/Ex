852d250831ffde3e7a1d1dba802925ce331a530f
Purvinis <g.purvinis@gmail.com>
2020-09-10 18:16:34 -1000

    added ggplot2

--- getData.R
+++ getData.R
@@ -11,6 +11,7 @@ library(dplyr)
 library(tidyr)
 library(lubridate)
 library(data.table)
+library(ggplot2)
 
 # dataset: Get zipped file from course website and put in current director
 # Then unzip it
